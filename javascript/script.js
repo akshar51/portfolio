c389991cf8ded3c4d774356c1a40396a599936f8
akshar66 <aksharparekh401@gmail.com>
2024-08-26 12:23:17 +0530

    done

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,17 @@
-
+// ANIMATED HEADLINE
 $(function() {
     $('.selector').animatedHeadline({
         animationType:"clip",
         
     });
+})
+
+
+
+// KURSOR
+
+new kursor({
+    type: 2,
+    removeDefaultCursor: true,
+    color: '#A1DD70',
 })
